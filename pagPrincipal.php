5bdfa45307c921c6ee6bc3fc9c6a12d5a38442ea
Gabriel Almeida <92796929+gw1803@users.noreply.github.com>
2021-10-31 15:05:43 -0300

    Add files via upload

---
+++ pagPrincipal.php
@@ -0,0 +1,23 @@
+ <html>	
+		<head>
+			<meta charset="utf-8"/>
+			<title> Among Us - Vida Real </title>
+			<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+			<link type="text/css" rel="stylesheet" href="css/bootstrap.css"/>
+			<link type="text/css" rel="stylesheet" href="css/estilo.css"/>	
+		</head>
+		<body>
+			<?php	
+				$pagina = "principal";
+				require_once('menu.php'); 
+			?>
+			<div class="container">				
+				<h5 id="titulo"> Pagina Principal </h5>
+				<img class="fotoLogo" src="imagens/logo_com_nome.png" alt="Imagem do morador">	
+			</div> 	
+			<script type="text/javascript" src="js/jquery.js"></script>
+			<script type="text/javascript" src="js/bootstrap.js"></script>
+			<script type="text/javascript" src="js/jquery.validate.js"></script>		
+			<script type="text/javascript" src="js/jquery.mask.js"></script>					
+		</body>
+</html>
